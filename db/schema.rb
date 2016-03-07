dac3085dae4d0db07bb86034f9043827c768dcf3
kscotteng <kscott@gmail.com>
2016-03-07 17:41:00 -0500

    add course

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307203811) do
+ActiveRecord::Schema.define(version: 20160307223901) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.integer  "course_id"
